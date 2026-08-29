705bca949d0bcc013cef1f04dbbf6dea959ddb64
aozaznsetr-lang <aozaznsetr@gmail.com>
2026-08-30 00:09:24 +0700

    Gi113-2026-1690703754

--- lab02/Program.cs
+++ lab02/Program.cs
@@ -40,30 +40,30 @@ namespace lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
 
-           // Console.WriteLine($"Name: {Aiden}");
-           // Console.WriteLine($"class: {Warrior}");
-           // Console.WriteLine($"HP: {400}");
-           // Console.WriteLine($"Speed: {60}");
-           // Console.Writeline($"attack; {84}");
-           // Console.Writeline($"luck; {50}");
-           // Console.WriteLine($"Name: {Orion}");
-           // Console.WriteLine($"class: {Wizar}");
-           // Console.WriteLine($"HP: {2500}");
-           // Console.WriteLine($"Speed: {40}");
-           // Console.Writeline($"attack; {120}");
-           // Console.Writeline($"luck; {70}");
-           // Console.WriteLine($"Name: {Raven}");
-           // Console.WriteLine($"class: {Assassin}");
-           // Console.WriteLine($"HP: {150}");
-           // Console.WriteLine($"Speed: {99}");
-           // Console.Writeline($"attack; {100}");
-           // Console.Writeline($"luck; {60}");
-           // Console.WriteLine($"Name: {Zane}");
-           // Console.WriteLine($"class: {Tank}");
-           // Console.WriteLine($"HP: {1000}");
-           // Console.WriteLine($"Speed: {25}");
-           // Console.Writeline($"attack; {30}");
-           // Console.Writeline($"luck; {31}");
+            Console.WriteLine($"Name: {Aiden}");
+            Console.WriteLine($"class: {Warrior}");
+            Console.WriteLine($"HP: {400}");
+            Console.WriteLine($"Speed: {60}");
+            Console.Writeline($"attack; {84}");
+            Console.Writeline($"luck; {50}");
+            Console.WriteLine($"Name: {Orion}");
+            Console.WriteLine($"class: {Wizar}");
+            Console.WriteLine($"HP: {2500}");
+            Console.WriteLine($"Speed: {40}");
+            Console.Writeline($"attack; {120}");
+            Console.Writeline($"luck; {70}");
+            Console.WriteLine($"Name: {Raven}");
+            Console.WriteLine($"class: {Assassin}");
+            Console.WriteLine($"HP: {150}");
+            Console.WriteLine($"Speed: {99}");
+            Console.Writeline($"attack; {100}");
+            Console.Writeline($"luck; {60}");
+            Console.WriteLine($"Name: {Zane}");
+            Console.WriteLine($"class: {Tank}");
+            Console.WriteLine($"HP: {1000}");
+            Console.WriteLine($"Speed: {25}");
+            Console.Writeline($"attack; {30}");
+            Console.Writeline($"luck; {31}");
         }
     }
 }
