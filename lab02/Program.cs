50bd75942e23c73717ace70f668354df4a5b5003
aozaznsetr-lang <aozaznsetr@gmail.com>
2026-09-01 15:42:25 +0700

    gi113-2026-1690703754

--- lab02/Program.cs
+++ lab02/Program.cs
@@ -40,47 +40,47 @@ namespace lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
 
-            string heroName = "Aiden";
+            string hero1Name = "Aiden";
             int hero1hp = 400;
             float hero1atk = 100;
             double hero1Speed = 60;
             char hero1luck = 84;
             Console.WriteLine("----- HERO 1 -----");
             Console.WriteLine($"Name: {hero1Name}");
-            Console.WriteLine($"HP: {hero1Hp}");
+            Console.WriteLine($"HP: {hero1hp}");
             Console.WriteLine($"atk: {hero1atk}");
             Console.WriteLine($"luck: {hero1luck}");
             
-            string heroName = "Orion";
-            int hero1hp = 450;
-            float hero1atk = 260;
-            double hero1Speed = 30;
-            char hero1luck = 44;
+            string hero2Name = "Orion";
+            int hero2hp = 450;
+            float hero2atk = 260;
+            double hero2Speed = 30;
+            char hero2luck = 44;
             Console.WriteLine("----- HERO 2 -----");
             Console.WriteLine($"Name: {hero2Name}");
-            Console.WriteLine($"HP: {hero2Hp}");
+            Console.WriteLine($"HP: {hero2hp}");
             Console.WriteLine($"atk: {hero2atk}");
             Console.WriteLine($"luck: {hero2luck}");
            
-            string heroName = "Raven";
-            int hero1hp = 200;
-            float hero1atk = 200;
-            double hero1Speed = 42;
-            char hero1luck = 36;
+            string hero3Name = "Raven";
+            int hero3hp = 200;
+            float hero3atk = 200;
+            double hero3Speed = 42;
+            char hero3luck = 36;
             Console.WriteLine("----- HERO 3 -----");
             Console.WriteLine($"Name: {hero3Name}");
-            Console.WriteLine($"HP: {hero3Hp}");
+            Console.WriteLine($"HP: {hero3hp}");
             Console.WriteLine($"atk: {hero3atk}");
             Console.WriteLine($"luck: {hero3luck}");
             
-            string heroName = "Zane";
+            string hero4Name = "Zane";
             int hero4hp = 600;
             float hero4atk = 140;
             double hero4Speed = 66;
             char hero4luck = 40;
             Console.WriteLine("----- HERO 4 -----");
             Console.WriteLine($"Name: {hero4Name}");
-            Console.WriteLine($"HP: {hero4Hp}");
+            Console.WriteLine($"HP: {hero4hp}");
             Console.WriteLine($"atk: {hero4atk}");
             Console.WriteLine($"luck: {hero4luck}");
         }
